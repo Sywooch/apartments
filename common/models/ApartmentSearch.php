e0f8da519f3cfe577da1d8f4e07d0c52cd010d27
Vlad Prybylov <prybylov.v@gmail.com>
2017-03-26 19:21:54 +0300

    Filters added

--- common/models/ApartmentSearch.php
+++ common/models/ApartmentSearch.php
@@ -13,11 +13,16 @@ use common\models\Apartment;
 class ApartmentSearch extends Apartment
 {
 
+    public $min_price;
+    public $max_price;
+    public $elevator;
+    public $apartment_id;
+
     public function rules()
     {
         return [
             [['id', 'description_ua', 'description_en', 'stock', 'room_count', 'bed_count', 'floor'], 'integer'],
-            [['title_ru', 'title_ua', 'title_en', 'description_ru', 'coordinates', 'type', 'area'], 'safe'],
+            [['title_ru', 'title_ua', 'title_en', 'description_ru', 'coordinates', 'type', 'area', 'min_price', 'max_price', 'elevator', 'apartment_id'], 'safe'],
             [['price_2', 'price_night', 'price_day', 'price_5', 'price_10', 'apartment_area'], 'number'],
         ];
     }
@@ -48,7 +53,15 @@ class ApartmentSearch extends Apartment
             return $dataProvider;
         }
 
-        // grid filtering conditions
+        if (is_array($this->type)) {
+            $query->andFilterWhere(['in', 'type', $this->type]);
+        }
+        else {
+            $query->andFilterWhere([
+                'type' => $this->type
+            ]);
+        }
+
         $query->andFilterWhere([
             'id' => $this->id,
             'description_ua' => $this->description_ua,
@@ -65,13 +78,18 @@ class ApartmentSearch extends Apartment
             'apartment_area' => $this->apartment_area,
         ]);
 
+        $query->joinWith(['facilities']);
+
         $query->andFilterWhere(['like', 'title_ru', $this->title_ru])
             ->andFilterWhere(['like', 'title_ua', $this->title_ua])
             ->andFilterWhere(['like', 'title_en', $this->title_en])
             ->andFilterWhere(['like', 'description_ru', $this->description_ru])
             ->andFilterWhere(['like', 'coordinates', $this->coordinates])
-            ->andFilterWhere(['like', 'type', $this->type])
-            ->andFilterWhere(['like', 'area', $this->area]);
+//            ->orFilterWhere(['like', 'type', $this->type])
+            ->andFilterWhere(['like', 'area', $this->area])
+            ->andFilterWhere(['>=', 'price_day', $this->min_price])
+            ->andFilterWhere(['<=', 'price_day', $this->max_price])
+            ->andFilterWhere(['like', 'facilities.elevator', $this->elevator]);
 
         return $dataProvider;
     }
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -47,12 +47,35 @@ $lang = Yii::$app->language;
 
             <?php Pjax::begin(['id' => 'new_country']); ?>
 
-            <?php $form = ActiveForm::begin(['method' => 'get','options' => ['data-pjax' => true ]]); ?>
+            <?php $form = ActiveForm::begin([ 'id' => 'test','method' => 'get','options' => ['data-pjax' => true ]]); ?>
 
-            <?= $form->field($searchModel, 'title_ru') ?>
+            <?= $form->field($searchModel, 'room_count')->dropDownList([
+                'prompt'=>'Выберите количество комнат...',
+                '1' => '1',
+                '2' => '2',
+                '3' => '3',
+                '4' => '4',
+                '5' => '5',
+            ]);
+            ?>
+
+            <?= $form->field($searchModel, 'bed_count')->dropDownList([
+                'prompt'=>'Выберите количество спальных мест...',
+                '1' => '1',
+                '2' => '2',
+                '3' => '3',
+                '4' => '4',
+                '5' => '5',
+                '6' => '6',
+                '7' => '7',
+                '8' => '8',
+                '9' => '9',
+                '10' => '10',
+            ]);
+            ?>
 
             <?= $form->field($searchModel, 'area')->dropDownList([
-                'prompt'=>'Выберите район...',
+                'prompt'=>'',
                 'Александровский' => 'Александровский',
                 'Заводской' => 'Заводской',
                 'Коммунарский' => 'Коммунарский',
@@ -63,8 +86,78 @@ $lang = Yii::$app->language;
             ]);
             ?>
 
+
+<!--            --><?//= $form->field($searchModel, 'type')->checkbox(['value' => 'Дом'])->label('Дом') ?>
+<!--            --><?//= $form->field($searchModel, 'type')->checkbox(['value' => 'Квартира'])->label('Квартира') ?>
+<!--            --><?//= $form->field($searchModel, 'type')->checkbox(['value' => 'Комната'])->label('Комната') ?>
+            <?= $form->field($searchModel, 'type')->checkboxList(['Дом'=>'Дом', 'Квартира'=>'Квартира', 'Комната'=>'Комната']) ?>
+
+            <?= $form->field($searchModel, 'stock')->checkbox(['uncheckValue'=>NULL]) ?>
             <div class="form-group">
-                <?= Html::submitButton('Apply', ['class' => 'btn btn-success']) ?>
+                <div class="col-lg-6">
+                    <?= $form->field($searchModel, 'min_price')->textInput()->label('От') ?>
+                </div>
+                <div class="col-lg-6">
+                    <?= $form->field($searchModel, 'max_price')->textInput()->label('До') ?>
+                </div>
+            </div>
+
+<!--            <h1 style="text-align: center">Удобства</h1><br><br>-->
+<!--            <div class="row">-->
+<!--                <div class="col-md-3">-->
+<!--    -->
+                    <?= $form->field($searchModel, 'elevator')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'internet')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'animals')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'kitchen')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'gym')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'intercom')->checkbox() ?>
+<!--                </div>-->
+<!--                <div class="col-md-3">-->
+<!--                    --><?//= $form->field($facilities, 'fireplace')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'waggon')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'heating')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'wifi')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'disabled')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'iron')->checkbox() ?>
+<!--                </div>-->
+<!--                <div class="col-md-3">-->
+<!--                    --><?//= $form->field($facilities, 'drying_machine')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'family')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'parking')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'washer_machine')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'hair_dryer')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'tv')->checkbox() ?>
+<!--                </div>-->
+<!--                <div class="col-md-3">-->
+<!--                    --><?//= $form->field($facilities, 'conditioner')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'cable_tv')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'smoke')->checkbox() ?>
+<!--    -->
+<!--                    --><?//= $form->field($facilities, 'separate_entrance')->checkbox() ?>
+<!--                </div>-->
+<!--            </div>-->
+
+            <div class="form-group">
+                <?= Html::submitButton('Поиск', ['class' => 'btn btn-success']) ?>
+                <?= Html::a('Сброс фильтров', ['index'], ['class' => 'btn btn-danger']) ?>
             </div>
 
         <?php ActiveForm::end(); ?>
@@ -73,6 +166,26 @@ $lang = Yii::$app->language;
             'dataProvider' => $dataProvider,
             'itemView' => '_apartment',
         ]);?>
+
+        <?php
+        $this->registerJs(
+            '$("#test").submit(function(e){
+                $("#test").find("select").each(function() {
+                    var select = $(this);
+                    if (!select.val() || select.val() == "prompt") {
+                        select.remove(); // or input.prop("disabled", true);
+                    }
+                });
+                $("#test").find("input[type=\'checkbox\']").each(function() {
+                $(\'input[type="hidden"]\').remove();
+                    var input = $(this);
+                    if (input.val() == 0) {
+                        input.remove(); // or input.prop("disabled", true);
+                    }
+                });
+            });'
+        );
+        ?>
         <?php Pjax::end(); ?>
     </div>
 </div>
